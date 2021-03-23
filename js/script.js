c4033ff36d22c7d15520a063bf7ce5ab5760b3e4
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-03-23 14:43:50 +0100

    FizzBuzz

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@
 // al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 
 // che di 5 stampi FizzBuzz.
 
+// Scrivo un programma che stampa i numeri da 1 a 100
+for (var i = 1; i <= 100; i++) {
+    
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz'); // Per i multipli di 3 e 5 stampo FizzBuzz
+    } else if (i % 3 == 0) {
+        console.log('Fizz'); // Per i multipli di 3 stampo Fizz
+    } else if (i % 5 == 0) {
+        console.log('Buzz'); // Per i multipli di 5 stampo Buzz
+    } else {
+        console.log(i); // Per gli altri numeri stampo il numero
+    }
+}
